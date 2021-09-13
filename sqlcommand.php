fed343ca3ec89fa6c5645ebe07b2da6eee219958
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-13 20:27:32 +0600

    ok

--- sqlcommand.php
+++ sqlcommand.php
@@ -1,6 +1,10 @@
 <?php
 $sql = "SELECT employee_id, department_id from job_history";
 // distinct thakle douplicate value return kore na
+//The SELECT DISTINCT statement is used to return only distinct (different) values.
 $sql = "SELECT DISTINCT manager_id from employees";
+// same sonkha 11 type er ache
+$sql = "SELECT COUNT(DISTINCT department_id) FROM employees"
+
 
 ?>
